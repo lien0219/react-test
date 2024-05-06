da7d769a57a3fb85a0c959c07718b462dc97381d
lien0219 <lien0001@163.com>
2024-05-06 22:54:03 +0800

    阅读账单时间选择

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -1,5 +1,69 @@
+import { NavBar, DatePicker } from "antd-mobile";
+import "./index.scss";
+
+import { useState } from "react";
+import classNames from "classnames";
+
 const Month = () => {
-  return <div>Month</div>;
+  // 时间弹窗
+  const [dateVisible, setDateVisible] = useState(false);
+  const onConfirm = () => {
+    setDateVisible(false);
+  };
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={() => setDateVisible(true)}>
+            <span className="text">2023|3月账单</span>
+            <span
+              className={classNames("arrow", dateVisible && "expand")}
+            ></span>
+          </div>
+          {/* 统计区域 */}
+          <div className="twoLineOverview">
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            onCancel={() => setDateVisible(false)}
+            onConfirm={onConfirm}
+            onClose={() => setDateVisible(false)}
+            max={new Date()}
+          />
+        </div>
+        {/* 单日列表统计 */}
+        {/* {dayGroup.keys.map((key) => {
+          return (
+            <DailyBill
+              key={key}
+              date={key}
+              billList={dayGroup.groupData[key]}
+            />
+          );
+        })} */}
+      </div>
+    </div>
+  );
 };
 
 export default Month;
