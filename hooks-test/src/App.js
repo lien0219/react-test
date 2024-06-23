191bdaafdd8bed66819cda341dddac4f8ab0c8b6
lien0219 <lien0001@163.com>
2024-06-23 19:58:10 +0800

    useReducer使用

--- hooks-test/src/App.js
+++ hooks-test/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "SET":
+      return action.payload;
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, 0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is App
+      <button onClick={() => dispatch({ type: "INC" })}>+</button>
+      {state}
+      <button onClick={() => dispatch({ type: "DEC" })}>-</button>
+      <button onClick={() => dispatch({ type: "SET", payload: 100 })}>
+        update
+      </button>
     </div>
   );
 }
