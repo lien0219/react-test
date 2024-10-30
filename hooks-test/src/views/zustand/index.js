6717ef3a4e36014b19309d8e83fd237f69df4866
lien0219 <lien0001@163.com>
2024-10-30 11:34:18 +0800

    zustand异步使用

--- hooks-test/src/views/zustand/index.js
+++ hooks-test/src/views/zustand/index.js
@@ -1,4 +1,7 @@
 import { create } from "zustand";
+import { useEffect } from "react";
+
+const URL = "http://geek.itheima.net/v1_0/channels";
 
 //创建store
 const useStore = create((set) => {
@@ -9,14 +12,32 @@ const useStore = create((set) => {
     inc: () => {
       set((state) => ({ count: state.count + 1 }));
     },
+    //异步使用
+    channelList: [],
+    fetchGetList: async () => {
+      const res = await fetch(URL);
+      const jsonRes = await res.json();
+      console.log(jsonRes);
+      set({
+        channelList: jsonRes.data.channels,
+      });
+    },
   };
 });
 
 export default function ZusAnd() {
-  const { count, inc } = useStore();
+  const { count, inc, channelList, fetchGetList } = useStore();
+  useEffect(() => {
+    fetchGetList();
+  }, [fetchGetList]);
   return (
     <>
       <button onClick={inc}>{count}</button>
+      <ul>
+        {channelList.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
