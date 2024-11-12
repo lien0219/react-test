e1abc5c2b47ca58b953475ead4a1622a804fc066
lien0219 <lien0001@163.com>
2024-11-12 21:46:16 +0800

    泛型传递

--- react-ts-pro/src/App.tsx
+++ react-ts-pro/src/App.tsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
+type User = {
+  name: string;
+  age: number;
+};
 function App() {
-  return <>app</>;
+  // const [value, toggle] = useState(false);
+
+  // const [list, setList] = useState([1, 2, 3]);
+
+  // const changeValue = () => {
+  //   toggle(true);
+  // };
+
+  // const changeList = () => {
+  //   setList([2]);
+  // };
+
+  //泛型传递
+  // const [user, setUser] = useState<User>({
+  //   name: "李四",
+  //   age: 18,
+  // });
+  const [user, setUser] = useState<User>(() => {
+    return {
+      name: "李四",
+      age: 18,
+    };
+  });
+
+  const changeUser = () => {
+    setUser({
+      name: "哈哈哈",
+      age: 28,
+    });
+  };
+
+  return (
+    <>
+      app{user.name}-{user.age}
+      <button onClick={changeUser}>改变user</button>
+    </>
+  );
 }
 
 export default App;
