05923b5289d7560351ced18f522582374914beb9
lien0219 <lien0001@163.com>
2024-11-16 17:03:01 +0800

    axios和ts的结合封装使用

---
+++ react-jikemobile-ts/src/apis/list.ts
@@ -0,0 +1,19 @@
+import { http } from "@/utils";
+import type { ResType } from "./shared";
+
+//接口类型
+type ChannelItem = {
+  id: number;
+  name: string;
+};
+
+type ChannelRes = {
+  channels: ChannelItem[];
+};
+
+//请求
+export function fetchChannelAPI() {
+  return http.request<ResType<ChannelRes>>({
+    url: "/channels",
+  });
+}
